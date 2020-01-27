f6be06dbf8ceebd0fbeea3c8a70ba2e1e3ca243d
thaislip <taber.haislip@gmail.com>
2020-01-27 15:58:36 -0500

    Updates

---
+++ precache-manifest.997e9a3fa1e08ee25c9b0454063353f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4f8992e03f6d35bfc77297f5f86a4b",
+    "url": "/thaislip/clicky-game.git/index.html"
+  },
+  {
+    "revision": "a31e215ee0abcd627f63",
+    "url": "/thaislip/clicky-game.git/static/css/main.1cec9da5.chunk.css"
+  },
+  {
+    "revision": "6c7b494d697009e39d11",
+    "url": "/thaislip/clicky-game.git/static/js/2.aa138e3e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/thaislip/clicky-game.git/static/js/2.aa138e3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a31e215ee0abcd627f63",
+    "url": "/thaislip/clicky-game.git/static/js/main.e3921d32.chunk.js"
+  },
+  {
+    "revision": "b0d3e141aacae4717b4c",
+    "url": "/thaislip/clicky-game.git/static/js/runtime-main.b1e6320b.js"
+  }
+]);
